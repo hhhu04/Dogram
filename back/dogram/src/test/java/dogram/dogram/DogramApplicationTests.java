2ccd9aefef4c43926b5a4ec3c0753b05135947a0
hhhu04 <hhhu04@gmail.com>
2021-04-19 19:00:48 +0900

    dogram

---
+++ back/dogram/src/test/java/dogram/dogram/DogramApplicationTests.java
@@ -0,0 +1,13 @@
+package dogram.dogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
